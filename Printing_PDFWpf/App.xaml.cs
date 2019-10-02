b4473667c896c73a84dda9a7304daf07107b4291
Zachary Reiss <zachary.reiss123@gmail.com>
2019-10-02 11:12:22 -0600

    Start adding viewmodels

--- Printing_PDFWpf/App.xaml.cs
+++ Printing_PDFWpf/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Printing_PDFWpf.Views;
+using Prism.Ioc;
+using Prism.Regions;
+using Prism.Unity;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -11,7 +15,21 @@ namespace Printing_PDFWpf
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+        //protected override void ConfigureDefaultRegionBehaviors(IRegionBehaviorFactory regionBehaviors)
+        //{
+        //    //regionBehaviors.A
+        //    base.ConfigureDefaultRegionBehaviors(regionBehaviors);
+        //}
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
     }
 }
--- Printing_PDFWpf/Views/MainWindow.xaml.cs
+++ Printing_PDFWpf/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Printing_PDFWpf
+namespace Printing_PDFWpf.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
