397a553d964436adda8e51ffb3b22961058e699a
Zachary Reiss <zachary.reiss123@gmail.com>
2019-10-02 10:47:31 -0600

    Add project files.

---
+++ Printing_PDFWpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printing_PDFWpf.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly IRegionManager regionManager;
+
+        public MainWindowViewModel( IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+        private DelegateCommand<string> _searchZipCommand;
+        public DelegateCommand<string> SearchZipCommand =>
+            _searchZipCommand ?? (_searchZipCommand = new DelegateCommand<string>(ExecuteSearchZipCommand));
+
+        void ExecuteSearchZipCommand(string parameter)
+        {
+            regionManager.RequestNavigate("ContentRegion", parameter);
+        }
+    }
+
+}
